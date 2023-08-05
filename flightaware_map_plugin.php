73f475b347d417e708a969cb904fba7523716723
waldspurgert <141451591+waldspurgert@users.noreply.github.com>
2023-08-05 02:23:11 -0400

    Update flightaware_map_plugin.php

--- flightaware_map_plugin.php
+++ flightaware_map_plugin.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Plugin Name: FlightAware Map Plugin
- * Description: A custom plugin to integrate FlightAware API and plot airplanes on a Google Map.
+ * Description: A custom plugin to integrate FlightAware API and plot airplanes on a Google Map or display them in a table format.
  * Version: 1.0
  * Author: Trevor Waldspurger
  */
@@ -97,54 +97,104 @@ function flightaware_map_settings_init() {
         'flightaware-map-settings',
         'flightaware_map_track_area_section'
     );
+
+    // Add a new section for the table format option
+    add_settings_section(
+        'flightaware_map_table_section',
+        'Table Format Settings',
+        'flightaware_map_table_section_callback',
+        'flightaware-map-settings'
+    );
+
+    // Add the new option field for the table format
+    add_settings_field(
+        'show_table_format',
+        'Show Planes in Table Format',
+        'show_table_format_field_callback',
+        'flightaware-map-settings',
+        'flightaware_map_table_section'
+    );
 }
 add_action('admin_init', 'flightaware_map_settings_init');
 
-// Callback function for the section
+// Callback function for the sections
 function flightaware_map_section_callback() {
     echo '<p>Enter your FlightAware API key and Google Maps API Key below:</p>';
+}
+
+function flightaware_map_track_area_section_callback() {
+    echo '<p>Enter latitude and longitude coordinates for the area you want to track planes.</p>';
 }
 
 // Callback functions for the fields
 function flightaware_api_key_field_callback() {
     $options = get_option('flightaware_map_settings');
-    $api_key = isset($options['flightaware_api_key']) ? $options['flightaware_api_key'] : 'cwICdVV8dCeS5gKqoUAOfWw25k169f2x';
+    $api_key = isset($options['flightaware_api_key']) ? $options['flightaware_api_key'] : '';
     echo '<input type="text" name="flightaware_map_settings[flightaware_api_key]" value="' . esc_attr($api_key) . '" />';
 }
 
 function google_maps_api_key_field_callback() {
     $options = get_option('flightaware_map_settings');
-    $google_maps_api_key = isset($options['google_maps_api_key']) ? $options['google_maps_api_key'] : 'YOUR_GOOGLE_MAPS_API_KEY';
+    $google_maps_api_key = isset($options['google_maps_api_key']) ? $options['google_maps_api_key'] : '';
     echo '<input type="text" name="flightaware_map_settings[google_maps_api_key]" value="' . esc_attr($google_maps_api_key) . '" />';
 }
 
-// Callback function for the custom area section
-function flightaware_map_track_area_section_callback() {
-    echo '<p>Enter latitude and longitude coordinates for the area you want to track planes.</p>';
-}
-
-// Callback functions for the custom area fields
 function custom_track_area_lat_field_callback() {
     $options = get_option('flightaware_map_settings');
-    echo '<input type="text" name="flightaware_map_settings[custom_track_area_lat]" value="' . esc_attr($options['custom_track_area_lat'] ?? '') . '" />';
+    $custom_track_area_lat = isset($options['custom_track_area_lat']) ? $options['custom_track_area_lat'] : '';
+    echo '<input type="text" name="flightaware_map_settings[custom_track_area_lat]" value="' . esc_attr($custom_track_area_lat) . '" />';
 }
 
 function custom_track_area_lng_field_callback() {
     $options = get_option('flightaware_map_settings');
-    echo '<input type="text" name="flightaware_map_settings[custom_track_area_lng]" value="' . esc_attr($options['custom_track_area_lng'] ?? '') . '" />';
+    $custom_track_area_lng = isset($options['custom_track_area_lng']) ? $options['custom_track_area_lng'] : '';
+    echo '<input type="text" name="flightaware_map_settings[custom_track_area_lng]" value="' . esc_attr($custom_track_area_lng) . '" />';
 }
 
-// Shortcode callback to display the map
-function flightaware_map_shortcode_callback() {
+function flightaware_map_table_section_callback() {
+    echo '<p>Select whether to show planes in table format or not.</p>';
+}
+
+function show_table_format_field_callback() {
+    $options = get_option('flightaware_map_settings');
+    $show_table_format = isset($options['show_table_format']) ? $options['show_table_format'] : '0';
+    echo '<input type="checkbox" name="flightaware_map_settings[show_table_format]" value="1" ' . checked(1, $show_table_format, false) . ' />';
+}
+
+// Shortcode callback to display the map or table format
+function flightaware_map_shortcode_callback($atts) {
     ob_start();
     $flight_data = flightaware_map_get_flight_data();
+
+    // Check if the table format option is enabled
+    $options = get_option('flightaware_map_settings');
+    $show_table_format = isset($options['show_table_format']) ? $options['show_table_format'] : '0';
+
     if ($flight_data) {
-        // Process and display the flight data on the map
-        // For example:
-        // echo '<div id="flightaware-map"></div>';
+        if (isset($atts['format']) && $atts['format'] === 'table') {
+            // Display the flight data in table format
+            echo '<table>';
+            echo '<thead><tr><th>Flight ID</th><th>Latitude</th><th>Longitude</th><th>Altitude</th></tr></thead>';
+            echo '<tbody>';
+            foreach ($flight_data['flights'] as $flight) {
+                echo '<tr>';
+                echo '<td>' . $flight['flight_id'] . '</td>';
+                echo '<td>' . $flight['latitude'] . '</td>';
+                echo '<td>' . $flight['longitude'] . '</td>';
+                echo '<td>' . $flight['altitude'] . '</td>';
+                echo '</tr>';
+            }
+            echo '</tbody>';
+            echo '</table>';
+        } else {
+            // Display the flight data on the map
+            // For example:
+            // echo '<div id="flightaware-map"></div>';
+        }
     } else {
         echo 'Unable to fetch flight data from FlightAware API.';
     }
+
     return ob_get_clean();
 }
 add_shortcode('flightaware_map', 'flightaware_map_shortcode_callback');
@@ -152,10 +202,12 @@ add_shortcode('flightaware_map', 'flightaware_map_shortcode_callback');
 // Function to fetch data from the FlightAware API
 function flightaware_map_get_flight_data() {
     $options = get_option('flightaware_map_settings');
-    $api_key = isset($options['flightaware_api_key']) ? $options['flightaware_api_key'] : 'cwICdVV8dCeS5gKqoUAOfWw25k169f2x';
+    $api_key = isset($options['flightaware_api_key']) ? $options['flightaware_api_key'] : '';
 
     // Customize the URL based on the FlightAware API endpoint and query parameters
-    $api_url = 'https://aeroapi.flightaware.com/aeroapi/endpoint?other_parameters=...';
+    $custom_track_area_lat = isset($options['custom_track_area_lat']) ? $options['custom_track_area_lat'] : '';
+    $custom_track_area_lng = isset($options['custom_track_area_lng']) ? $options['custom_track_area_lng'] : '';
+    $api_url = 'https://aeroapi.flightaware.com/aeroapi/v3/your-endpoint-here?lat=' . $custom_track_area_lat . '&lng=' . $custom_track_area_lng;
 
     // Set up the request arguments
     $request_args = array(
